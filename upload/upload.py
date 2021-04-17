a2399605d6ce4ecaf2249dec968d810393c4dceb
son991 <80301571+son991@users.noreply.github.com>
2021-04-17 16:41:18 +0900

    업로드 파트입니다
    추가 설명은 readme에 저장해 놓았습니다

---
+++ upload/upload.py
@@ -0,0 +1,34 @@
+import datetime
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+credentials = flow.run_console()
+youtube = build('youtube', 'v3', credentials=credentials)
+
+upload_date_time = datetime.datetime(2021, 4, 17, 0, 0, 0).isoformat() + '.000Z'
+
+request_body = {
+    'snippet': {
+        'categoryI': 19,
+        'title': ' upload test',
+        'description': 'anyvideo',
+        'tags': ['fortest', 'Youtube API']
+    },
+    'status': {
+        'privacyStatus': 'private',
+        'publishAt': upload_date_time,
+        'selfDeclaredMadeForKids': False, 
+    }
+}
+
+mediaFile = MediaFileUpload('1.avi')
+
+response_upload = youtube.videos().insert(
+    part='snippet,status',
+    body=request_body,
+    media_body=mediaFile
+).execute()
