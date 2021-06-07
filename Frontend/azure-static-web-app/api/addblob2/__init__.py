6a2bf27f69e26378cdfe40e5d3822f10d0dce12b
kcsol <kcso721@naver.com>
2021-06-07 15:58:59 +0900

    connection str changed

--- Frontend/azure-static-web-app/api/addblob2/__init__.py
+++ Frontend/azure-static-web-app/api/addblob2/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     video=req.files['videoFile']
     print(img.filename)
     print(video.filename)
-    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=sh0909storage;AccountKey=Sg/73AcJ6ah/DP38yZ087H4YBSXc0irmBKZd2C5o3I6eFhDWhQeH1zAJ45U3f9d86CdYJVaeY5wRWarKoF1QoA==;EndpointSuffix=core.windows.net") # this is my table key
+    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=storageforys;AccountKey=sTNZfBUGl7EbNxi7duoUuzXnBuWPdfVIcgn4HzDu2y8q6BVz9oJNxr0XkJD2lFmZBsNHVpSbz8rbEzLFLZHfbQ==;EndpointSuffix=core.windows.net") # this is my table key
     blob_client = blob_service_client.get_blob_client(container="imgforyoutube", blob= img.filename)
     blob_client.upload_blob(img.stream.read())
     blob_client = blob_service_client.get_blob_client(container = "fileforyoutube", blob = video.filename)
