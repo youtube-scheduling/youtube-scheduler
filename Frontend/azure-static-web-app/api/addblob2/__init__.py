b43cb2c34ea6ec5c38059771ecdad712f59d5fbf
SH0909 <poa0909@gmail.com>
2021-06-07 15:44:54 +0900

    addblob 수정

--- Frontend/azure-static-web-app/api/addblob2/__init__.py
+++ Frontend/azure-static-web-app/api/addblob2/__init__.py
@@ -5,42 +5,15 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    name = req.files['imgFile']
+    img = req.files['imgFile']
     video=req.files['videoFile']
-    print(name.filename)
+    print(img.filename)
     print(video.filename)
-    upload_blob(video,name)
+    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=sh0909storage;AccountKey=Sg/73AcJ6ah/DP38yZ087H4YBSXc0irmBKZd2C5o3I6eFhDWhQeH1zAJ45U3f9d86CdYJVaeY5wRWarKoF1QoA==;EndpointSuffix=core.windows.net") # this is my table key
+    blob_client = blob_service_client.get_blob_client(container="imgforyoutube", blob= img.filename)
+    blob_client.upload_blob(img.stream.read())
+    blob_client = blob_service_client.get_blob_client(container = "fileforyoutube", blob = video.filename)
+    blob_client.upload_blob(video.stream.read())
+
     return func.HttpResponse("success")
 
-def upload_blob(video, img):
-    #path = os.getcwd()
-    path = './data'
-
-
-    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=sh0909storage;AccountKey=Sg/73AcJ6ah/DP38yZ087H4YBSXc0irmBKZd2C5o3I6eFhDWhQeH1zAJ45U3f9d86CdYJVaeY5wRWarKoF1QoA==;EndpointSuffix=core.windows.net") # this is my table key
-    container_name = "fileforyoutube"
-    container_name_img = "imgforyoutube"
-
-    #initial code for create blob container
-    #container_client = blob_service_client.create_container(container_name)
-    #container_client = blob_service_client.create_container(container_name_img)
-
-    #str(uuid.uuid4()) is the name of file
-    #local_file_name = str(uuid.uuid4()) + ".avi"
-    #upload_file_path = os.path.join(path, local_file_name)
-    upload_file_path=video.filename
-    #video.save(upload_file_path)
-
-    #blob_client = blob_service_client.get_blob_client(container = container_name, blob = local_file_name)
-    #blob_client.upload_blob(upload_file_path)
-
-    #local_file_name = str(uuid.uuid4()) + ".img"
-    #upload_file_path_img = os.path.join(path, local_file_name)
-    #img.save(upload_file_path_img)
-
-    #blob_client = blob_service_client.get_blob_client(container=container_name_img, blob= local_file_name)
-    #blob_client.upload_blob(upload_file_path_img)
-
-    print('blob success')
-
-    #await asyncio.sleep(1)
